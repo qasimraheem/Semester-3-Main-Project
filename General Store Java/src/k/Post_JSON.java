7bfa5798f88a0e7165b87d2c9e03561f9522413b
qasimraheem <qasimmehmood13936@gmail.com>
2018-04-01 11:28:58 +0500

    external liberies for java API

--- General Store Java/src/k/Post_JSON.java
+++ General Store Java/src/k/Post_JSON.java
@@ -13,11 +13,11 @@ public class Post_JSON {
     }
     public static void Post_JSON() {
         String query_url = "https://gurujsonrpc.appspot.com/guru";
-        String json = "{ \"method\" : \"guru.test\", \"params\" : [ \"jinu awad\" ], \"id\" : 123 }";
+        String json = "{ \"method\" : \"guru.test\", \"params\" : [ \"jinuawad\" ], \"id\" : 123 }";
         try {
             URL url = new URL(query_url);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(5000);
+            conn.setConnectTimeout(50000);
             conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -28,7 +28,7 @@ public class Post_JSON {
             // read the response
             InputStream in = new BufferedInputStream(conn.getInputStream());
             String result = IOUtils.toString(in, "UTF-8");
-            System.out.println(result);
+            System.out.println(result+"testing");
             System.out.println("result after Reading JSON Response");
             JSONObject myResponse = new JSONObject(result);
             System.out.println("jsonrpc- "+myResponse.getString("jsonrpc"));
